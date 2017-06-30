14a567120a7d4f9834a3284b63ed3f1c4c23a4ee
sidsdirector <directorcomercial@sids.com.co>
2017-06-30 17:15:34 -0500

    Mejora visual y creación funcion Js
    Se mejora el margen para tamaño mobil, se activa la clase hover en las tablas como ayuda visual para activar columna cuando esta diligenciando datos y creación parcialmente de funciones Js para agregar elementos <option> dentro del <select>

---
+++ 1/js/script.js
@@ -0,0 +1,149 @@
+
+function llenar (){
+
+	var opciones = document.getElementById('ingreso').value;
+	var selector =  document.getElementById('ie');
+	var crear = document.createElement('option');
+
+	for (var i = 0; i <= opciones; i++ )
+	{
+		selector.options[0] = new Option (i,'valor :'+ i);
+		/*crear.text="kiwi";
+		selector.options.add(crear,1);*/
+	}
+}
+
+function mostrar(){
+	document.getElementById('oculto').style.display = 'block';
+	document.getElementById('mostrar').style.display = 'none';
+
+}
+
+function mostrar2(){
+	document.getElementById('oculto2').style.display = 'block';
+	document.getElementById('oculto').style.display = 'none';
+
+}
+
+function mostrar3(){
+	document.getElementById('oculto3').style.display = 'block';
+	document.getElementById('oculto2').style.display = 'none';
+
+}
+
+function mostrar4(){
+	document.getElementById('oculto4').style.display = 'inline';
+	document.getElementById('oculto3').style.display = 'none';
+}
+
+function mostrar5(){
+	document.getElementById('oculto5').style.display = 'inline';
+	document.getElementById('oculto4').style.display = 'none';
+}
+
+function mostrar6(){
+	document.getElementById('oculto6').style.display = 'inline';
+	document.getElementById('oculto5').style.display = 'none';
+}
+
+function mostrar7(){
+	document.getElementById('oculto7').style.display = 'inline';
+	document.getElementById('oculto6').style.display = 'none';
+}
+
+function mostrar8(){
+	document.getElementById('oculto8').style.display = 'inline';
+	document.getElementById('oculto7').style.display = 'none';
+}
+
+function mostrar9(){
+	document.getElementById('oculto9').style.display = 'inline';
+	document.getElementById('oculto8').style.display = 'none';
+}
+
+function mostrar10(){
+	document.getElementById('oculto10').style.display = 'inline';
+	document.getElementById('oculto9').style.display = 'none';
+}
+
+function mostrar11(){
+	document.getElementById('oculto11').style.display = 'inline';
+	document.getElementById('oculto10').style.display = 'none';
+}
+
+function mostrar12(){
+	document.getElementById('oculto12').style.display = 'inline';
+	document.getElementById('oculto11').style.display = 'none';
+}
+
+function mostrar13(){
+	document.getElementById('oculto13').style.display = 'inline';
+	document.getElementById('oculto12').style.display = 'none';
+}
+
+function mostrar13(){
+	document.getElementById('oculto13').style.display = 'inline';
+	document.getElementById('oculto12').style.display = 'none';
+}
+
+function mostrar14(){
+	document.getElementById('oculto14').style.display = 'inline';
+	document.getElementById('oculto13').style.display = 'none';
+}
+
+function mostrar15(){
+	document.getElementById('oculto15').style.display = 'inline';
+	document.getElementById('oculto14').style.display = 'none';
+}
+
+function mostrar16(){
+	document.getElementById('oculto16').style.display = 'inline';
+	document.getElementById('ps').style.display = 'inline';
+	document.getElementById('psa').style.display = 'inline';
+	document.getElementById('ri').style.display = 'inline';
+	document.getElementById('oculto15').style.display = 'none';
+}
+
+function mostrar17(){
+	document.getElementById('oculto17').style.display = 'inline';
+	document.getElementById('oculto16').style.display = 'none';
+}
+
+function mostrar18(){
+	document.getElementById('oculto18').style.display = 'inline';
+	document.getElementById('oculto17').style.display = 'none';
+}
+
+function mostrar19(){
+	document.getElementById('oculto19').style.display = 'inline';
+	document.getElementById('oculto18').style.display = 'none';
+}
+
+function mostrar20(){
+	document.getElementById('oculto20').style.display = 'inline';
+	document.getElementById('oculto19').style.display = 'none';
+}
+
+function mostrar21(){
+	document.getElementById('oculto21').style.display = 'inline';
+	document.getElementById('oculto20').style.display = 'none';
+}
+
+function mostrar22(){
+	document.getElementById('oculto22').style.display = 'inline';
+	document.getElementById('oculto21').style.display = 'none';
+}
+
+
+/*function revelar() {
+    var x = document.getElementById('gram');
+
+    x.options[1]= document.getElementById('ps').style.display = 'inline';
+	x.options[2]=document.getElementById('psa').style.display = 'inline';
+	x.options[3]=document.getElementById('ri').style.display = 'inline';
+            }*/
+
+
+        
+
+
